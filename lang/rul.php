f8eff77cdfc8061b497f3f0aa21a2fc93d895e1f
Soz Nov <soz@simpleorder.com>
2016-01-31 11:17:04 +0200

    Convert to UTF-8: rul

--- lang/rul.php
+++ lang/rul.php
@@ -1,142 +1,136 @@
 <?
+$changed = 'web';
 
-$changed="web";
+$poisk = 'Budet vam urokom, nečego iskatĩ čerez <b>'.$poisk . '</b> vsĩakuju jeresĩ tipa <b><cite>"' . $text . '"</cite></b>.';
 
+$codepage = '';
 
+$lj_user = '';
 
+$other = '';
 
-$poisk="Budet vam urokom, ne&#269;ego iskat&#297; &#269;erez <b>poiskovik</b> vs&#297;akuju jeres&#297; tipa <b><u>\"text\"</u></b>.";
+$email = '';
 
-$codepage="";
+$password = 'zazimojo';
 
-$lj_user="";
+$headpage = 'NACHUJ ZDĚSĨ';
 
-$other="";
+$epigraph = '"Malenĩkij šag dlĩa odnogo čelověka - bolĩšoj šag dlĩa vsego čelověčestva." ©N.Armstrong<br>"Let my people go!" ©L.Armstrong';
 
-$email="";
+$media = '';
 
-$password="zazimojo";
+$head = 'NACHUJ';
 
-$headpage="NACHUJ ZD&#282;S&#296;";
+$official_site = 'Oficialĩnyj sajt sýmvoličeskogo napravlenija';
 
-$epigraph="\"Malen&#297;kij &#353;ag dl&#297;a odnogo &#269;elov&#283;ka - bol&#297;&#353;oj &#353;ag dl&#297;a vsego &#269;elov&#283;&#269;estva.\" &copy;N.Armstrong<br>\"Let my people go!\" &copy;L.Armstrong";
+$national_office = 'Russkij latiničeskij ofis';
 
-$media="";
+$oi_chto_eto = 'Oj, čto ęto?!';
 
-$head="NACHUJ";
+$zdes_raspolojeno = 'Zděsĩ raspoloženo oficialĩnoje predstavitelĩstvo NACHUJ v internetě. Sĩuda posylajut.';
 
-$official_site="Oficial&#297;nyj sajt s&#253;mvoli&#269;eskogo napravlenija";
+$chto_eto_znachit = 'Čto že ęto značit?!';
 
-$national_office="Russkij latini&#269;eskij ofis";
+$vas_poslali = 'Jesli vy okazalisĩ na ętoj stranicě, ęto označajet tolĩko odno: vas poslali nachuj. Grubo, no ęlegantno: prislav ętu ssylku.';
 
-$oi_chto_eto="Oj, &#269;to &#281;to?!";
+$kak_eto_moglo = 'Kak že takoje moglo slučitĩsĩa so mnoju?!';
 
-$zdes_raspolojeno="Zd&#283;s&#297; raspolo&#382;eno oficial&#297;noje predstavitel&#297;stvo NACHUJ v internet&#283;. S&#297;uda posylajut.";
+$vot_samye = 'Vot samyje rasprostranennyje pričiny:';
 
-$chto_eto_znachit="&#268;to &#382;e &#281;to zna&#269;it?!";
+$prichiny = 'Vy utomili sobesědnika glupymi voprosami, prosĩbami ili sovětami.
+Vy obiděli sobesědnika neostorožnym vyskazyvanijem: zaděli jego religioznyje, političeskije, muzykalĩnyje i pročije vzglĩady i vkusy, libo inym obrazom vtorglisĩ v jego vnutrennij mir i podvergli kritikě to, čěm on dorožit.
+Vam namekajut o tom, čto ne vernut dolg, ne zaplatĩat za rabotu, otkazyvajut v seksualĩnoj blizosti.
+S vami prosto ne chotĩat obşatĩsĩa. Takoje tože byvajet.';
 
-$vas_poslali="Jesli vy okazalis&#297; na &#281;toj stranic&#283;, &#281;to ozna&#269;ajet tol&#297;ko odno: vas poslali nachuj. Grubo, no &#281;legantno: prislav &#281;tu ssylku.";
+$est_variant = 'Jestĩ, konečno, variant, čto vy popali na ętu stranicu slučajno, najdĩa jeje, napriměr, v poiskovikě. =POISK= Zadumajtesĩ: bytĩ možet, vam nado serĩjezno peresmotrětĩ svoji vzglĩady na žiznĩ, jesli daže poiskoviki stali po svojej iniciativě predlagatĩ vam pojti nachuj?';
 
-$kak_eto_moglo="Kak &#382;e takoje moglo slu&#269;it&#297;s&#297;a so mnoju?!";
+$chto_delat = 'Čto že mně teperĩ dělatĩ?!';
 
-$vot_samye="Vot samyje rasprostranennyje pri&#269;iny:";
+$sovetuem = 'Sovětujem:';
 
-$prichiny="Vy utomili sobes&#283;dnika glupymi voprosami, pros&#297;bami ili sov&#283;tami.
-Vy obid&#283;li sobes&#283;dnika neostoro&#382;nym vyskazyvanijem: zad&#283;li jego religioznyje, politi&#269;eskije, muzykal&#297;nyje i pro&#269;ije vzgl&#297;ady i vkusy, libo inym obrazom vtorglis&#297; v jego vnutrennij mir i podvergli kritik&#283; to, &#269;&#283;m on doro&#382;it.
-Vam namekajut o tom, &#269;to ne vernut dolg, ne zaplat&#297;at za rabotu, otkazyvajut v seksual&#297;noj blizosti.
-S vami prosto ne chot&#297;at ob&#351;at&#297;s&#297;a. Takoje to&#382;e byvajet.";
+$sovety = 'Peresmotrětĩ svoje otnošenije k ętomu čelověku.
+Vserĩjez na nego obidětĩsĩa.
+Postavitĩ zakladku, i v slědujuşij raz poslatĩ ssylku tomu, kogo chotite poslatĩ nachuj vy.';
 
-$est_variant="Jest&#297;, kone&#269;no, variant, &#269;to vy popali na &#281;tu stranicu slu&#269;ajno, najd&#297;a jeje, naprim&#283;r, v poiskovik&#283;. =POISK= Zadumajtes&#297;: byt&#297; mo&#382;et, vam nado ser&#297;jezno peresmotr&#283;t&#297; svoji vzgl&#297;ady na &#382;izn&#297;, jesli da&#382;e poiskoviki stali po svojej iniciativ&#283; predlagat&#297; vam pojti nachuj?";
+$kak_mne_jit = 'Kak mně teperĩ žitĩ?!';
 
-$chto_delat="&#268;to &#382;e mn&#283; teper&#297; d&#283;lat&#297;?!";
+$zapomnite = 'Zapomnite: vy ne pervyj, kogo poslali nachuj, i ne poslědnij. Ęta stranica byla otkryta v pĩatnicu 13 ijunĩa 2008 goda, i nachuj sĩuda poslali uže rovno count čelověk.
+Osteregajtesĩ poddělok: ęta stranica iznačalĩnaja i jedinstvennaja. My postojanno soveršenstvujem jeje.
+Znajte: blagodarĩa osobomu interaktivnomu ustrojstvu, vsĩakij raz, kogda kto-nibudĩ idet nachuj, v štab-kvartirě Oficialĩnogo napravlenija razdajetsĩa iz kolonok tichij pečalĩnyj vzdoch. My v kursě vašich pochoždenij. Nachuj lĩubit vas!';
 
-$sovetuem="Sov&#283;tujem:";
+$bottom_vernutsa = 'Vernutĩsĩa nazad';
 
-$sovety="Peresmotr&#283;t&#297; svoje otno&#353;enije k &#281;tomu &#269;elov&#283;ku.
-Vser&#297;jez na nego obid&#283;t&#297;s&#297;a.
-Postavit&#297; zakladku, i v sl&#283;duju&#351;ij raz poslat&#297; ssylku tomu, kogo chotite poslat&#297; nachuj vy.";
-
-$kak_mne_jit="Kak mn&#283; teper&#297; &#382;it&#297;?!";
-
-$zapomnite="Zapomnite: vy ne pervyj, kogo poslali nachuj, i ne posl&#283;dnij. &#280;ta stranica byla otkryta v p&#297;atnicu 13 ijun&#297;a 2008 goda, i nachuj s&#297;uda poslali u&#382;e rovno count &#269;elov&#283;k.
-Osteregajtes&#297; podd&#283;lok: &#281;ta stranica izna&#269;al&#297;naja i jedinstvennaja. My postojanno sover&#353;enstvujem jeje.
-Znajte: blagodar&#297;a osobomu interaktivnomu ustrojstvu, vs&#297;akij raz, kogda kto-nibud&#297; idet nachuj, v &#353;tab-kvartir&#283; Oficial&#297;nogo napravlenija razdajets&#297;a iz kolonok tichij pe&#269;al&#297;nyj vzdoch. My v kurs&#283; va&#353;ich pocho&#382;denij. Nachuj l&#297;ubit vas!";
-
-$bottom_vernutsa="Vernut&#297;s&#297;a nazad";
-
-$bottom_vernut="&#268;to vy, kak malen&#297;kij?
+$bottom_vernut = 'Čto vy, kak malenĩkij?
 ##
-Ots&#297;uda je&#351;e nikto ne vozvra&#351;als&#297;a.";
+Otsĩuda jeşe nikto ne vozvraşalsĩa.';
 
-$bottom_izbrannoe="Dobavit&#297; v Izbrannoje";
+$bottom_izbrannoe = 'Dobavitĩ v Izbrannoje';
 
-$bottom_izbr="Pozdravl&#297;ajem! Vy - izbrannyj!";
+$bottom_izbr = 'Pozdravlĩajem! Vy - izbrannyj!';
 
-$bottom_start="Sd&#283;lat&#297; stranicu startovoj";
+$bottom_start = 'Sdělatĩ stranicu startovoj';
 
-$bottom_strt="Vas ne pugajet risk uchud&#353;it&#297; svoje nastrojenije i samooc&#283;nku?
+$bottom_strt = 'Vas ne pugajet risk uchudšitĩ svoje nastrojenije i samoocěnku?
 ##
-Zachodite v men&#297;u \'Nastrojki obozr&#283;vatel&#297;a\'
-i d&#283;lajte stranicu startovoj.
-Nikto za vas &#281;to ne sd&#283;lajet!";
+Zachodite v menĩu "Nastrojki obozrěvatelĩa"
+i dělajte stranicu startovoj.
+Nikto za vas ęto ne sdělajet!';
 
-$bottom_druga="Poslat&#297; druga";
+$bottom_druga = 'Poslatĩ druga';
 
-$bottom_drug="&#268;toby poslat&#297; druga, otprav&#297;te jemu &#281;tu ssylku po &#281;lektronnoj po&#269;t&#283; ili po ICQ.
-My za vas &#281;to sd&#283;lat&#297; ne mo&#382;em - propadet &#281;ffekt.
+$bottom_drug = 'Čtoby poslatĩ druga, otpravĩte jemu ętu ssylku po ęlektronnoj počtě ili po ICQ.
+My za vas ęto sdělatĩ ne možem - propadet ęffekt.
 ##
-Uda&#269;i v dru&#382;b&#283;!";
+Udači v družbě!';
 
-$about="o &nbsp; p r o j e k t &#283; &nbsp; N A C H U J";
+$about = 'o   p r o j e k t ě   N A C H U J';
 
-$perevod="&copy;";
+$perevod = '©';
 
-$perevodchik="Gaius Marcus Victorinus";
+$perevodchik = 'Gaius Marcus Victorinus';
 
-$lj_zdra="priv&#283;tstvujet teb&#297;a,";
+$lj_zdra = 'privětstvujet tebĩa,';
 
-$lj_raz="-j raz";
+$lj_raz = '-j raz';
 
-$hello_you="";
+$hello_you = '';
 
-$hello_noprichina="";
+$hello_noprichina = '';
 
-$hello_nosovet="";
+$hello_nosovet = '';
 
-$otvetstvenno="";
+$otvetstvenno = '';
 
-$post="";
+$post = '';
 
-$post_="";
+$post_ = '';
 
-$postprivet="";
+$postprivet = '';
 
-$post_name="";
+$post_name = '';
 
-$post_mail="";
+$post_mail = '';
 
-$post_html="";
+$post_html = '';
 
-$post_ip="";
+$post_ip = '';
 
-$post_button="";
+$post_button = '';
 
-$post_alert="";
+$post_alert = '';
 
-$post_hidden="";
+$post_hidden = '';
 
-$disclamer="";
+$disclamer = '';
 
-$lleo="";
+$lleo = '';
 
-$lleo_url="";
+$lleo_url = '';
 
-$e_head="";
+$e_head = '';
 
-$e_text="";
+$e_text = '';
 
-$e_submit="";
+$e_submit = '';
 
-$e_comment="";
-
-?>
+$e_comment = '';
