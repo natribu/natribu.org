d2b6691604c1e146e935dec516107335d2d2a904
Soz Nov <soz@simpleorder.com>
2016-01-31 09:42:53 +0200

    Convert to UTF-8: de

--- lang/de.php
+++ lang/de.php
@@ -1,97 +1,77 @@
 <?
+$headpage = 'VERPISS DICH ist hier';
 
-$password="bashnbHDS";
+$epigraph = '"Ein kleiner Schritt fÃ¼r einen Mann -- ein groÃŸer Schritt fÃ¼r die ganze Menschheit." Â© N.Armstrong<br>"Let my people go!" Â© L.Armstrong';
+$media = 'de.swf';
 
-//SONNY
-// + "nik" <nikvetrov@mail.ru>
-//    ó ÄÅÔÓÔ×Á ÚÎÁÀ ÎÅÍÅÃËÉÊ, ÎÅÓËÏÌØËÏ ÌÅÔ ĞÒÏ×ÅÌ × ÷ÅÎÅ, ÎÏ ×ÏÔ ÒÁÄ ÕÚÎÁÔØ, ËÁË 
-//    éíåîîï ÂÕÄÅÔ ĞÏÓÌÁÔØ ÎÁ ÈÕÊ ĞÏ-ÎÅÍÅÃËÉ. =)) éÔÁË, ×ÏÔ ŞÔÏ ĞÏÄËÏÒÒÅËÔÉÒÏ×ÁÔØ 
-//    × ĞÅÒÅ×ÏÄÅ, Ó×ÅÒÈÕ ×ÎÉÚ:
+$head = 'VERPISS DICH';
+$official_site = 'Internetauftritt einer symbolischen Richtung';
 
+$national_office = 'Deutsches BÃ¼ro';
 
-$headpage="VERPISS DICH ist hier";
+$oi_chto_eto = 'Was ist das denn?!';
+$zdes_raspolojeno = 'Hier befindet sich die offizielle Vertretung von VERPISS DICH im Internet. Hierhin wird man geschickt.';
 
-$epigraph="\"Ein kleiner Schritt f&uuml;r einen Mann -- ein gro&szlig;er Schritt f&uuml;r die ganze Menschheit.\" &copy; N.Armstrong<br>\"Let my people go!\" &copy; L.Armstrong";
-$media="de.swf";
+$chto_eto_znachit = 'Was bedeutet das denn?!';
+$vas_poslali = 'Wenn Sie auf dieser Seite gelangt sind, bedeutet das nur eins: jemand hat zu Ihnen gesagt, verpiss Dich. Frech aber elegant: indem man Ihnen diesen Verweis schickte.';
 
-$head="VERPISS DICH";
-$official_site="Internetauftritt einer symbolischen Richtung";
-//offizielle Seite der symbolischen Richtung
-$national_office="Deutsches B&uuml;ro";
+$kak_eto_moglo = 'Wie konnte mir das denn passieren?!';
+$vot_samye = 'Hier sind die am haufigsten auftreffenden Grunde gelistet:';
 
-$oi_chto_eto="Was ist das denn?!";
-$zdes_raspolojeno="Hier befindet sich die offizielle Vertretung von VERPISS DICH im Internet. Hierhin wird man geschickt.";
+$prichiny = 'Sie haben Ihren Ansprechspartner mit dummen Fragen, Bitten oder RatschlÃ¤gen ermÃ¼det.
+Sie haben Ihren Ansprechspartner mit einer rÃ¼cksichtsloser Sentenz beleidigt: seine religiosen, politischen, musikalischen oder andere Meinungen und Geschmacke verletzt oder auf eine andere Weise in seine innere Welt eingedrungen sind und etwas kritiziert was ihm teuer ist.
+Es wird darauf hingewiesen, dass Ihnen: die Schulden nicht zurÃ¼ckgezahlt werden, die Arbeit nicht bezahlt wird, oder kein sexueller Kontakt angeboten wird.
+Man will mit Ihnen einfach nichts zu tun haben. So etwas passiert auch.';
+$est_variant = 'Es besteht natÃ¼rlich auch die MÃ¶glichkeit, dass Sie durch einen Zufall auf diese Seite gekommen sind, z.B. durch eine Suchmachine. Denken Sie nach: es kÃ¶nnte sein, dass Sie Ihre persÃ¶nlichen Werte einer grÃ¼ndlichen Revision unterziehen sollten, wenn Ihnen selbst die armen Suchmachinen vorschlagen, sich zu verpissen?';
 
-$chto_eto_znachit="Was bedeutet das denn?!";
-$vas_poslali="Wenn Sie auf dieser Seite gelangt sind, bedeutet das nur eins: jemand hat zu Ihnen gesagt, verpiss Dich. Frech aber elegant: indem man Ihnen diesen Verweis schickte.";
-
-$kak_eto_moglo="Wie konnte mir das denn passieren?!";
-$vot_samye="Hier sind die am haufigsten auftreffenden Grunde gelistet:";
-
-$prichiny="Sie haben Ihren Ansprechspartner mit dummen Fragen, Bitten oder Ratschl&auml;gen erm&uuml;det.
-Sie haben Ihren Ansprechspartner mit einer r&uuml;cksichtsloser Sentenz beleidigt: seine religiosen, politischen, musikalischen oder andere Meinungen und Geschmacke verletzt oder auf eine andere Weise in seine innere Welt eingedrungen sind und etwas kritiziert was ihm teuer ist.
-Es wird darauf hingewiesen, dass Ihnen: die Schulden nicht zur&uuml;ckgezahlt werden, die Arbeit nicht bezahlt wird, oder kein sexueller Kontakt angeboten wird.
-Man will mit Ihnen einfach nichts zu tun haben. So etwas passiert auch.";
-$est_variant="Es besteht nat&uuml;rlich auch die M&ouml;glichkeit, dass Sie durch einen Zufall auf diese Seite gekommen sind, z.B. durch eine Suchmachine. Denken Sie nach: es k&ouml;nnte sein, dass Sie Ihre pers&ouml;nlichen Werte einer gr&uuml;ndlichen Revision unterziehen sollten, wenn Ihnen selbst die armen Suchmachinen vorschlagen, sich zu verpissen?";
-
-$chto_delat="Was mach' ich denn jetzt blo&szlig;?!";
-$sovetuem="Ratschl&auml;ge:";
-$sovety="Sehen Sie Ihre Einstellung gegen&uuml;ber dieser Person nochmal durch.
+$chto_delat = 'Was mach\' ich denn jetzt bloÃŸ?!';
+$sovetuem = 'RatschlÃ¤ge:';
+$sovety = 'Sehen Sie Ihre Einstellung gegenÃ¼ber dieser Person nochmal durch.
 Werden Sie auf ihn echt sauer.
-Speichern Sie die Seite unter Ihren Favoriten und schicken Sie sie sp&auml;ter jemandem der sich nach Ihrer Meinung verpissen soll.";
-// Wenn Ihr gespr&auml;chspartner das geschriebene Wort schwer begreift, schicken Sie Ihm einen Link f&uuml;r Blinde, eine Zeichnung oder ein Logo; w&auml;hlen Sie das Passende in unserer <a href=media/media.shtml>Gallerie</a>
+Speichern Sie die Seite unter Ihren Favoriten und schicken Sie sie spÃ¤ter jemandem der sich nach Ihrer Meinung verpissen soll.';
 
-$kak_mne_jit="Wie soll ich jetzt weiter leben?!";
-$zapomnite="Eins sollen Sie wissen: Sie sind nicht der erste, der sich verpissen musste, und auch nicht der letzte. Diese Seite wurde am 12 Dezember 2005 ver&ouml;ffentlicht, und es haben sich seitdem bereits $count verpisst.
-Achten Sie auf F&auml;lschungen: diese Seite ist die einzige, originelle und urspr&uuml;ngliche. Wir arbeiten st&auml;ndig daran, sie noch besser zu machen.
-Hinweis: mit Hilfe eines speziellen interaktiven Ger&auml;tes, jedes Mal, dass sich jemand verpisst, erklingt in der Hauptquartier der Offiziellen Richtung ein leiserer Seufzer aus den Lautsprechern. Wir wissen &uuml;ber Ihre Abenteuer bescheid. Verpiss Dich liebt Sie!";
+$kak_mne_jit = 'Wie soll ich jetzt weiter leben?!';
+$zapomnite = 'Eins sollen Sie wissen: Sie sind nicht der erste, der sich verpissen musste, und auch nicht der letzte. Diese Seite wurde am 12 Dezember 2005 verÃ¶ffentlicht, und es haben sich seitdem bereits $count verpisst.
+Achten Sie auf FÃ¤lschungen: diese Seite ist die einzige, originelle und ursprÃ¼ngliche. Wir arbeiten stÃ¤ndig daran, sie noch besser zu machen.
+Hinweis: mit Hilfe eines speziellen interaktiven GerÃ¤tes, jedes Mal, dass sich jemand verpisst, erklingt in der Hauptquartier der Offiziellen Richtung ein leiserer Seufzer aus den Lautsprechern. Wir wissen Ã¼ber Ihre Abenteuer bescheid. Verpiss Dich liebt Sie!';
 
 
-// êíîïêè
-
-$bottom_vernutsa="Zur&uuml;ck";
-$bottom_vernut="Seien Sie doch kein Kind.
-Von hier kam noch Keiner zur&uuml;ck.
+$bottom_vernutsa = 'ZurÃ¼ck';
+$bottom_vernut = 'Seien Sie doch kein Kind.
+Von hier kam noch Keiner zurÃ¼ck.
 ##
-Tut uns Leid.";
+Tut uns Leid.';
 
+$bottom_izbrannoe = 'Zu den Favoriten hinzufÃ¼gen';
+$bottom_izbr = 'Gratuliere! Sie sind ein Favorit!';
 
-$bottom_izbrannoe="Zu den Favoriten hinzuf&uuml;gen";
-$bottom_izbr="Gratuliere! Sie sind ein Favorit!";
-
-$bottom_start="Als Startseite setzen";
-$bottom_strt="Sind Sie bereit Ihre Laune
-und Selbstwertgef&uuml;hl auf`s Spiel zu setzen?
+$bottom_start = 'Als Startseite setzen';
+$bottom_strt = 'Sind Sie bereit Ihre Laune
+und SelbstwertgefÃ¼hl auf`s Spiel zu setzen?
 ##
-W&auml;hlen Sie den Men&uuml;punkt &lt;&lt;Internet Einstellungen&gt;&gt;
+WÃ¤hlen Sie den MenÃ¼punkt <<Internet Einstellungen>>
 und setzen Sie dies als Ihre Startseite.
 ##
-Keiner wird es f&uuml;r Sie machen!";
+Keiner wird es fÃ¼r Sie machen!';
 
 
-$bottom_druga="Verpissen Sie einen Freund";
-$bottom_drug="Um das zu tun, schicken Sie ihm diesen Link per E-mail oder ICQ.
-Wenn wir das f&uuml;r Sie tun, ist leider der Effekt weg.
+$bottom_druga = 'Verpissen Sie einen Freund';
+$bottom_drug = 'Um das zu tun, schicken Sie ihm diesen Link per E-mail oder ICQ.
+Wenn wir das fÃ¼r Sie tun, ist leider der Effekt weg.
 ##
-Viel Erfolg in Ihrer Freundschaft!";
+Viel Erfolg in Ihrer Freundschaft!';
 
-// îñòàëüíîå
+$about = 'A B O U T Â  P R O J E C T';
+$perevod = 'Â©Â Ãœbersetzung:';
+$perevodchik = 'Sonny + Nikvetrov';
 
-$about="A B O U T &nbsp; P R O J E C T";
-$perevod="&copy;&nbsp;&Uuml;bersetzung:";
-$perevodchik="Sonny + Nikvetrov";
-
-// post.shtml
-
-$post="";
-$post_="";
-$postprivet="";
-$post_name="";
-$post_mail="";
-$post_html="";
-$post_ip="";
-$post_button="";
-$post_alert="";
-$post_hidden="";
-
-?>
+$post = '';
+$post_ = '';
+$postprivet = '';
+$post_name = '';
+$post_mail = '';
+$post_html = '';
+$post_ip = '';
+$post_button = '';
+$post_alert = '';
+$post_hidden = '';
